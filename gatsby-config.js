5acdf370d8263f6a8a7cd574a7b1fe4110c5a893
anamst <ana.milic.strkalj@gmail.com>
2024-12-02 13:05:46 +0100

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `Crypsmas`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
